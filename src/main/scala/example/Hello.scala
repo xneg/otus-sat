801496090c3e92b47c98fd6e6560f8ddaf0510c4
Evgeny Bikkinin <e.bikkinin@dodopizza.com>
2020-06-17 13:41:49 +0300

    Read csv file

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,17 @@
 package example
+import scala.io.Source
 
-object Hello extends Greeting with App {
-  println(greeting)
+object Hello extends Misc with App {
+  // SBT - это больно https://github.com/sbt/sbt/issues/3963
+  val filename = System.getProperty("user.dir") + "/src/main/resources/SAT__College_Board__2010_School_Level_Results.csv"
+  val lines = readFile(filename)
 }
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+trait Misc {
+  def readFile(filename: String): Seq[String] = {
+      val bufferedSource = io.Source.fromFile(filename)
+      val lines = (for (line <- bufferedSource.getLines()) yield line).toList
+      bufferedSource.close
+      lines
+  }
 }
